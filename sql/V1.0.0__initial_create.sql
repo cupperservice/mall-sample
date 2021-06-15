fbefa2927beefa411af6e5d38becfea91941dd2c
Kazuhisa Kawashima <cupper@mac.com>
2021-06-15 11:27:49 +0900

    add sql

---
+++ sql/V1.0.0__initial_create.sql
@@ -0,0 +1,23 @@
+CREATE TABLE users (
+  id          INT NOT NULL AUTO_INCREMENT,
+  login_id    VARCHAR(100) NOT NULL,
+  password    VARCHAR(100) NOT NULL,
+  name        VARCHAR(100) NOT NULL,
+  created_at  TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  updated_at  TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE tweet (
+  id          INT NOT NULL AUTO_INCREMENT,
+  owner_id    INT NOT NULL,
+  content     VARCHAR(255) NOT NULL,
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE hash_tag (
+  id          INT NOT NULL AUTO_INCREMENT,
+  tag         VARCHAR(50) NOT NULL,
+  tweet_id    INT NOT NULL,
+  PRIMARY KEY (id)
+);
